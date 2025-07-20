0941659f7b211114395693c3cb586e3b115682db
Lazymodthai <lazymodthai@gmail.com>
2025-07-21 02:04:54 +0700

    add: payment

--- src/components/booking/NumberField.tsx
+++ src/components/booking/NumberField.tsx
@@ -23,7 +23,7 @@ function NumberField(props: Props) {
           e.preventDefault();
         }
       }}
-      value={props.value}
+      value={props.value || ""}
       slotProps={{
         input: {
           inputProps: {
