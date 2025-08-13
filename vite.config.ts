2699fd6e650340303eb03ab3c67f81a0a03e14cf
anucha-liefcapital <anucha@liefcapital.com>
2025-08-13 10:23:59 +0700

    add: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
   // server: {
-  //   host: true,
-  //   allowedHosts: [
-  //     'artico.in.th'
-  //   ],
-  //   hmr: {
-  //     host: 'artico.in.th',
-  //     protocol: 'wss'
-  //   }
+  //   host: '0.0.0.0',
+  //   port: 8080,
+  //   hmr: true,
   // }
+  server: {
+    host: true,
+    allowedHosts: [
+      'artico.in.th'
+    ],
+    hmr: {
+      host: 'artico.in.th',
+      protocol: 'wss'
+    }
+  }
 })
