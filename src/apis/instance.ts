1dcef82cecb5fae568806c1bbc61291dc4fb743c
anucha-liefcapital <anucha@liefcapital.com>
2026-01-15 10:49:07 +0700

    fix: auth credential

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const isDev = import.meta.env.MODE === "development";
 
 const instanceAuth = axios.create({
-  baseURL: isDev ? "http://localhost:3005/" : import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  baseURL: isDev ? "http://localhost:3200/" : import.meta.env.VITE_BASE_URL,
   timeout: 10000,
   headers: {
     Accept: "application/json",
@@ -30,13 +31,13 @@ const instancePrices = axios.create({
 });
 
 const instanceUpload = axios.create({
-  baseURL: "http://localhost:3005/",
+  baseURL: "http://localhost:3200/",
   timeout: 10000,
   // No need to set Content-Type header - Axios will automatically set it to multipart/form-data with boundary when sending FormData
 });
 
 const instanceBookingManage = axios.create({
-  baseURL: "http://localhost:3005/",
+  baseURL: "http://localhost:3200/",
   timeout: 10000,
   headers: {
     Accept: "application/json",
